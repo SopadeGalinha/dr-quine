a2c496d7bdf776e8cc7ab570e3ae28f92a25a7b4
Jhonata <jhsocial@icloud.com>
2025-12-24 08:39:29 +0000

    C: Grace

---
+++ C/Grace.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Grace
+
+#define FT "Grace_kid.c"
+#define S "#include <stdio.h>%1$c%1$c// Grace%1$c%1$c#define FT %2$c%4$s%2$c%1$c#define S %2$c%3$s%2$c%1$c#define MAIN() int main() { FILE *f = fopen(FT, %2$cw%2$c); if (f) { fprintf(f, S, 10, 34, S, FT); fclose(f); } }%1$cMAIN()%1$c"
+#define MAIN() int main() { FILE *f = fopen(FT, "w"); if (f) { fprintf(f, S, 10, 34, S, FT); fclose(f); } }
+MAIN()
