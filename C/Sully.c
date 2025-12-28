bb59089114a5e601a05931c86857ed07f01821fb
Jhonata <jhsocial@icloud.com>
2025-12-28 22:29:04 +0000

    C: Sully

--- C/Sully.c
+++ C/Sully.c
@@ -6,7 +6,7 @@
 int main(void)
 {
     int i = 5;
-    char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c#include <fcntl.h>%1$c%1$cint main(void)%1$c{%1$c    int i = %2$d;%1$c    char *s = %3$c%4$s%3$c;%1$c    int fd;%1$c%1$c    if (i <= 0)%1$c        return (0);%1$c%1$c    if ((fd = open(%3$cSully_5.c%3$c, O_RDONLY)) != -1)%1$c    {%1$c        close(fd);%1$c        i--;%1$c    }%1$c%1$c    char filename[50];%1$c    sprintf(filename, %3$cSully_%%d.c%3$c, i);%1$c%1$c    if ((fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644)) != -1)%1$c    {%1$c        dprintf(fd, s, 10, i, 34, s);%1$c        close(fd);%1$c    }%1$c%1$c    char cmd[100];%1$c    sprintf(cmd, %3$cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d%3$c, i, i);%1$c    system(cmd);%1$c%1$c    sprintf(cmd, %3$c./Sully_%%d%3$c, i);%1$c    system(cmd);%1$c    return (0);%1$c}";
+    char *s = "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <unistd.h>%1$c#include <fcntl.h>%1$c%1$cint main(void)%1$c{%1$c    int i = %2$d;%1$c    char *s = %3$c%4$s%3$c;%1$c    int fd;%1$c%1$c    if (i <= 0)%1$c        return (0);%1$c%1$c    if ((fd = open(%3$cSully_5.c%3$c, O_RDONLY)) != -1)%1$c    {%1$c        close(fd);%1$c        i--;%1$c    }%1$c%1$c    char filename[50];%1$c    sprintf(filename, %3$cSully_%%d.c%3$c, i);%1$c%1$c    if ((fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644)) != -1)%1$c    {%1$c        dprintf(fd, s, 10, i, 34, s);%1$c        close(fd);%1$c    }%1$c%1$c    char cmd[100];%1$c    sprintf(cmd, %3$cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d%3$c, i, i);%1$c    system(cmd);%1$c%1$c    sprintf(cmd, %3$c./Sully_%%d%3$c, i);%1$c    system(cmd);%1$c    return (0);%1$c}%1$c";
     int fd;
 
     if (i <= 0)
