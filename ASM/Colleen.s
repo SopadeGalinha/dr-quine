aa0e3ac0556653f8a07abd3f14119bb1af017b44
Jhonata <jhsocial@icloud.com>
2025-12-29 11:22:44 +0000

    ASM: Comments with /**/

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,10 +1,12 @@
-; Outside comment
+; /*
+;     This program will print its own source when run.
+; */
 
 global main
 extern printf
 
 section .rodata
-s db "; Outside comment%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$cs db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfunc:%1$clea rax, [rel s]%1$cret%1$c%1$cmain:%1$center 0, 0%1$c; Inside comment%1$ccall func%1$cmov rdi, rax%1$cmov rsi, 10%1$cmov rdx, 34%1$cmov rcx, rax%1$cmov rax, 0%1$ccall printf%1$cleave%1$cret%1$c", 0
+s db "; /*%1$c;     This program will print its own source when run.%1$c; */%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$cs db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfunc:%1$clea rax, [rel s]%1$cret%1$c%1$cmain:%1$center 0, 0%1$c; /*%1$c; This program will print its own source when run.%1$c; */%1$ccall func%1$cmov rdi, rax%1$cmov rsi, 10%1$cmov rdx, 34%1$cmov rcx, rax%1$ccall printf%1$cmov rax, 0%1$cleave%1$cret%1$c", 0
 
 section .text
 func:
@@ -13,13 +15,15 @@ ret
 
 main:
 enter 0, 0
-; Inside comment
+; /*
+; This program will print its own source when run.
+; */
 call func
 mov rdi, rax
 mov rsi, 10
 mov rdx, 34
 mov rcx, rax
-mov rax, 0
 call printf
+mov rax, 0
 leave
 ret
