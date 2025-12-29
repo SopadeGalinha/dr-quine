4847878c1161c9510123c048b61937a9985a82e4
Jhonata <jhsocial@icloud.com>
2025-12-29 11:17:50 +0000

    C: Comments with /**/

--- C/Grace.c
+++ C/Grace.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/* This program will print its own source when run */
+/*
+    This program will print its own source when run.
+*/
 
 #define FT "Grace_kid.c"
-#define S "#include <stdio.h>%1$c%1$c/* This program will print its own source when run */%1$c%1$c#define FT %2$c%4$s%2$c%1$c#define S %2$c%3$s%2$c%1$c#define MAIN() int main() { FILE *f = fopen(FT, %2$cw%2$c); if (f) { fprintf(f, S, 10, 34, S, FT); fclose(f); } }%1$cMAIN()%1$c"
+#define S "#include <stdio.h>%1$c%1$c/*%1$c    This program will print its own source when run.%1$c*/%1$c%1$c#define FT %2$c%4$s%2$c%1$c#define S %2$c%3$s%2$c%1$c#define MAIN() int main() { FILE *f = fopen(FT, %2$cw%2$c); if (f) { fprintf(f, S, 10, 34, S, FT); fclose(f); } }%1$cMAIN()%1$c"
 #define MAIN() int main() { FILE *f = fopen(FT, "w"); if (f) { fprintf(f, S, 10, 34, S, FT); fclose(f); } }
 MAIN()
