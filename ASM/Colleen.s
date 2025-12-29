fd8e630fbd8f7719bff0c5b06f2f3503456b1802
Jhonata <jhsocial@icloud.com>
2025-12-29 22:02:32 +0000

    ASM: Improve robustness

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -6,7 +6,7 @@ global main
 extern printf
 
 section .rodata
-s db "; /*%1$c;     Outter: This program will print its own source when run.%1$c; */%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$cs db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfunc:%1$clea rax, [rel s]%1$cret%1$c%1$cmain:%1$center 0, 0%1$c; /*%1$c;     Inner: This program will print its own source when run.%1$c; */%1$ccall func%1$cmov rdi, rax%1$cmov rsi, 10%1$cmov rdx, 34%1$cmov rcx, rax%1$ccall printf%1$cmov rax, 0%1$cleave%1$cret%1$c", 0
+s db "; /*%1$c;     Outter: This program will print its own source when run.%1$c; */%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$cs db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$cfunc:%1$clea rax, [rel s]%1$cret%1$c%1$cmain:%1$cpush rbp%1$cmov rbp, rsp%1$csub rsp, 16%1$c; /*%1$c;     Inner: This program will print its own source when run.%1$c; */%1$ccall func%1$cmov rdi, rax%1$cmov rsi, 10%1$cmov rdx, 34%1$cmov rcx, rax%1$cxor rax, rax%1$ccall printf%1$cadd rsp, 16%1$cpop rbp%1$cmov rax, 0%1$cret%1$c", 0
 
 section .text
 func:
@@ -14,7 +14,9 @@ lea rax, [rel s]
 ret
 
 main:
-enter 0, 0
+push rbp
+mov rbp, rsp
+sub rsp, 16
 ; /*
 ;     Inner: This program will print its own source when run.
 ; */
@@ -23,7 +25,9 @@ mov rdi, rax
 mov rsi, 10
 mov rdx, 34
 mov rcx, rax
+xor rax, rax
 call printf
+add rsp, 16
+pop rbp
 mov rax, 0
-leave
 ret
